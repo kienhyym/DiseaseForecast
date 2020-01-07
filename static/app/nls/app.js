bb54c1fd24489bc9fbe3a168cc5991ccf9c0410f
kiennd <kiennd@healthchain.vn>
2020-01-07 09:20:44 +0700

    update

--- static/app/nls/app.js
+++ static/app/nls/app.js
@@ -11,10 +11,10 @@ define({
         "NO_RECORDS_FOUND":"No data yet",
         "no_records_found":"No data yet",
         "create_medical":"Đặt Khám",
-        "QUAN_LY_NGUOI_DUNG_CAP_DUOI":"Subordinate user management",
+        "QUAN_LY_NGUOI_DUNG_CAP_DUOI":"User management",
         "QUAN_LY_TIN_NHAN": "Message management",
-        "DANH_SACH_THONG_BAO_TU_D_MOSS": "List of notification from D-MOSS",
+        "DANH_SACH_THONG_BAO_TU_D_MOSS": "Notification from D-MOSS",
         "DANH_SACH_LOAI_THONG_BAO": "List of notification types",
-        "QUAN_LY_DON_VI": "Subordinate Unite management",
+        "QUAN_LY_DON_VI": "Unite management",
     }
 });
